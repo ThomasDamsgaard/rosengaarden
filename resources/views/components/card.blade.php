c2dcf4efd9cd140fde8f0c27c70712c9a5bae255
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2020-02-13 07:36:05 +0000

    Added horizontal rule to staff card

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -3,6 +3,7 @@
     <img src="{{ $img }}" class="card-img-top img-fluid" alt="">
     <div class="card-body text-center">
       <h5 class="card-title mb-0">{{ $name }}</h5>
+      <hr style="background-color: #C75B3A; height: 1px; margin: 0.25rem;">
       <div class="card-text text-black-50 mb-2">{{ $profession }}</div>
       <button type="button" class="btn btn-sm btn-rosengaarden">Læs mere</button>
     </div>
