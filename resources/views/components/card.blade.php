5d41dd63680db87f36a0165b3ee9a8305e8c223d
ThomasDamsgaard <thomas.damsgaard@hotmail.com>
2020-02-13 07:49:06 +0000

    Staff card image fix

--- resources/views/components/card.blade.php
+++ resources/views/components/card.blade.php
@@ -1,11 +1,14 @@
 <div id="{{ $id }}" class="col-xl-3 col-md-4 mb-4" data-toggle="modal" data-target="#staff-modal{{ $id }}" style="cursor: pointer;">
   <div class="card border-0 shadow">
-    <img src="{{ $img }}" class="card-img-top img-fluid" alt="">
-    <div class="card-body text-center">
-      <h5 class="card-title mb-0">{{ $name }}</h5>
-      <hr style="background-color: #C75B3A; height: 1px; margin: 0.25rem;">
-      <div class="card-text text-black-50 mb-2">{{ $profession }}</div>
-      <button type="button" class="btn btn-sm btn-rosengaarden">Læs mere</button>
+    <div style="height: 100%">
+      <img src="{{ $img }}" class="card-img-top img-fluid" alt="">
+      <div class="card-body text-center">
+        <h5 class="card-title mb-0">{{ $name }}</h5>
+        <hr style="background-color: #C75B3A; height: 1px; margin: 0.25rem;">
+        <div class="card-text text-black-50 mb-2">{{ $profession }}</div>
+        <button type="button" class="btn btn-sm btn-rosengaarden">Læs mere</button>
+      </div>
     </div>
+
   </div>
 </div>
